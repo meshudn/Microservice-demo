c151c4161d959e59e0f7f3501d098e819828d8a8
Meshu Deb Nath <meshu.uiu@gmail.com>
2021-01-06 13:14:41 +0100

    navbar and homepage component added

---
+++ frontend/src/components/homepage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../App.css';
+import axios from 'axios';
+import Product from './product';
+
+
+class Homepage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            products: {},
+            isCartMenuClick: false,
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3052/products')
+            .then(response => {
+                const products = response.data;
+                console.log(JSON.stringify(products));
+                //console.log(products[1].product_id);
+                this.setState({
+                    products : products
+                })
+            });
+    }
+
+    render() {
+        const productlist = [];
+        if(this.state.isCartMenuClick){
+
+        }
+        try {
+            var temp = this.state.products;
+            temp.map(function (index) {
+                return productlist.push(<Product  key={index.product_id} img={index.product_image} name={index.product_name} price={index.price}/>);
+            });
+        }
+        catch{
+            console.log('conversation happening');
+        }
+
+        return (
+            <div className="homepage">
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                        {productlist}
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default Homepage;
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+class Navbar extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <header className="container">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">WebShop (Microservice Demo) <span className="smallFont">Prototype</span></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">My Orders</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/carts">Carts</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
+export default Navbar;
+
+
