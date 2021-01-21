4a672c0a668694c9fb4f959597467191ea2ed81f
Meshu Deb Nath <meshu.uiu@gmail.com>
2021-01-21 22:27:34 +0100

    inventory service edited

--- inventoryservice/app.js
+++ inventoryservice/app.js
@@ -4,9 +4,41 @@ const port = 3052;
 var mysql = require('mysql');
 const cors = require('cors');
 var bodyParser = require('body-parser')
+var amqp = require('amqplib/callback_api');
 
 app.use(cors());
 app.use(bodyParser.json());
+/*
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'orderqueue';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+            if(msg.content.toString() === "order"){
+                console.log("new database rule set");
+            }
+        }, {
+            noAck: true
+        });
+    });
+});
+*/
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -35,7 +67,7 @@ app.get('/products', cors(), (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Cart Service listening at http://localhost:${port}`)
+    console.log(`Inventory Service listening at http://localhost:${port}`)
 });
 
 
